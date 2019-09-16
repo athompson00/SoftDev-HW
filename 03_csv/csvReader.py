350409da5577d6fc08434eac48c4d46f9f87ebc4
athompson00 <athompson00@stuy.edu>
2019-09-16 00:24:48 -0400

    csv reader assignment with function for random occupation

---
+++ 03_csv/csvReader.py
@@ -0,0 +1,19 @@
+import csv
+import random
+
+occupations = {}
+with open('occupations.csv', mode = 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    for row in csv_reader:
+        occupations[row["Job Class"]] = float(row["Percentage"])
+
+job = random.randint(0, 997)
+
+def randomJob():
+    total = 0;
+    for key in occupations:
+        total += occupations[key]
+        if (total * 10 >= job):
+            return key
+
+print(randomJob())
