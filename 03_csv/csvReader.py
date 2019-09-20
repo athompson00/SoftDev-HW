9f7b0a4055bc0c547b500faf427a0951abaae5b7
athompson00 <43051976+athompson00@users.noreply.github.com>
2019-09-20 09:19:35 -0400

    Update csvReader.py

--- 03_csv/csvReader.py
+++ 03_csv/csvReader.py
@@ -7,7 +7,9 @@ with open('occupations.csv', mode = 'r') as csv_file:
     for row in csv_reader:
         occupations[row["Job Class"]] = float(row["Percentage"])
 
-job = random.randint(0, 997)
+job = random.randint(0, 997)  ##ignores last row because of 997
+
+
 
 def randomJob():
     total = 0;
