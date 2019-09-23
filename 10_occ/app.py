ec091bd89b31ef044fe7b4e320f923808641df39
athompson00 <athompson00@stuy.edu>
2019-09-23 09:27:25 -0400

    print out random job and joblist on webapp

--- 10_occ/app.py
+++ 10_occ/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import csv
+import random
 app = Flask(__name__) #create instance of class Flask
 
 @app.route("/") #assign following fxn to run when root route requested
@@ -6,10 +8,32 @@ def hello_world():
     print(__name__) #where will this go?
     return "no hablo queso";
 
-@app.route("/stub")
+@app.route("/occupyflaskst")
 def stub():
-    nums = [2,1,5,6,7,4,3,2,34,5];
-    return render_template("stub.html", nums=nums);
+    job = randomJob()
+    occupations = get_occupations()
+    for occupation in occupations:
+        occupations[occupation] = str(occupations[occupation])
+    return render_template("stub.html", job = job, occupations = occupations);
+
+
+def get_occupations():
+    occupations = {}
+    with open('./data/occupations.csv', mode = 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            occupations[row["Job Class"]] = float(row["Percentage"])
+    return occupations
+
+def randomJob():
+    occupations = get_occupations()
+    job = random.randint(0, 997)  ##ignores last row because of 997
+    total = 0;
+    for key in occupations:
+        total += occupations[key]
+        if (total * 10 >= job):
+            return key
+
 
 if __name__ == "__main__":
     app.debug = True
