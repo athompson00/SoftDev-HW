95189de391723b21b125a385ba555b0d2f5093b7
athompson00 <athompson00@stuy.edu>
2019-09-26 09:30:29 -0400

    Added features when no name is inputted

--- 12_form/app.py
+++ 12_form/app.py
@@ -4,14 +4,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def occupation():
+    #print (request.args["username"])
     return render_template('test.html')
 
 @app.route("/auth")
 def stuff():
     print(request.form)
+    print ("????????")
+    print(request.method)
     print(app)
     print(request)
-    return render_template('auth.html', username = request.args["username"])
+    if request.args["username"] != "":
+        return render_template('auth.html', username = request.args["username"])
+    else:
+        return render_template('noname.html')
+
 
 
 if __name__ == "__main__":
