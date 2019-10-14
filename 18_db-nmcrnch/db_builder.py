69673a30774c70e73c8c157237f56640630ee29a
athompson00 <athompson00@stuy.edu>
2019-10-14 18:03:40 -0400

    started 18

---
+++ 18_db-nmcrnch/db_builder.py
@@ -0,0 +1,47 @@
+#Alex "Thumb" Thompson
+#SoftDev pd. 2
+#skeleton :: SQLITE3 BASICS
+#Oct 2019
+
+import sqlite3   #enable control of an sqlite database
+import csv       #facilitate CSV I/O
+
+
+DB_FILE="discobandit.db"
+
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops
+
+#==========================================================
+
+# < < < INSERT YOUR POPULATE-THE-DB CODE HERE > > >
+
+
+command = "CREATE TABLE students (name TEXT PRIMARY KEY, age INTEGER, id INTEGER);"          # test SQL stmt in sqlite3 shell, save as string
+c.execute(command)    # run SQL statement
+
+with open ("students.csv", "r") as csvfile :
+    filereader = csv.DictReader(csvfile)
+    for row in filereader:
+        command = "INSERT INTO students VALUES (" + "'" + row["name"] + "',"  + row["age"] + "," + row["id"] + ");"
+        #print(command)
+        c.execute(command)
+
+
+
+
+
+command = "CREATE TABLE courses (code, mark INTEGER, id INTEGER);"
+c.execute(command)
+
+with open ("courses.csv", "r") as csvfile :
+    filereader = csv.DictReader(csvfile)
+    for row in filereader:
+        command = "INSERT INTO courses VALUES (" "'" + row["code"] + "', " + row["mark"] + "," + row["id"] + ");"
+        #print(command)
+        c.execute(command)
+
+#==========================================================
+
+db.commit() #save changes
+db.close()  #close database
