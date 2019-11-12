76d65c25fa6211de54b4ab9dd5be340e226bca23
athompson00 <athompson00@stuy.edu>
2019-11-12 09:28:31 -0500

    first use of mars-rover api

---
+++ 24_rest/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import urllib, json
+
+app = Flask(__name__) #create instance of class Flask
+
+@app.route("/") #assign following fxn to run when root route requested
+def root():
+    u = urllib.request.urlopen("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=100&api_key=W56Oyc2HC3Kpl9qXvqmkIawDe3MeQbaFe053pgJF")
+    response = u.read()
+    data = json.loads(response)
+    print(data["camera"])
+    return "No hablo queso!"
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
