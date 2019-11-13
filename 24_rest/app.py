9f34623f07951d4328b6c06f6586062b447906f9
athompson00 <athompson00@stuy.edu>
2019-11-12 21:03:34 -0500

    completed assignment

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import urllib, json
 
 app = Flask(__name__) #create instance of class Flask
@@ -8,8 +8,8 @@ def root():
     u = urllib.request.urlopen("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=100&api_key=W56Oyc2HC3Kpl9qXvqmkIawDe3MeQbaFe053pgJF")
     response = u.read()
     data = json.loads(response)
-    print(data["photos"][0]['img_src'])
-    return "No hablo queso!"
+    image = data["photos"][0]['img_src']
+    return render_template("index.html", pic = image)
 
 
 if __name__ == "__main__":
