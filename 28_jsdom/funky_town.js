a2283236720488ff1f41a82c40790c541aad9aa5
athompson00 <athompson00@stuy.edu>
2019-12-11 19:12:05 -0500

    completed assignment and added color changing

--- 28_jsdom/funky_town.js
+++ 28_jsdom/funky_town.js
@@ -51,10 +51,49 @@ var randomStudent = function(){
 
 
 var factButton = document.getElementById("factButton").addEventListener("click",
-  function factB(){
-    var number = parseInt(document.getElementById("factInput").value);
-    console.log(fact(number));
-    return fact(number);
-  })
+    function factB(){
+      var number = parseInt(document.getElementById("factInput").value);
+      console.log(fact(number));
+      document.getElementById("factAnswer").innerHTML = fact(number);
+      document.getElementById("factAnswer").style.color = "green";
+      document.getElementById("factA").style.color = "green";
+      return fact(number);
+    }
+);
+
+var fibButton = document.getElementById("fibButton").addEventListener("click",
+    function fibB(){
+      var number = parseInt(document.getElementById("fibInput").value);
+      console.log(fibonacci(number));
+      document.getElementById("fibAnswer").innerHTML = fibonacci(number);
+      document.getElementById("fibAnswer").style.color = "green";
+      document.getElementById("fibA").style.color = "green";
+      return fibonacci(number);
+    }
+);
+
+var studentButton = document.getElementById("studentButton").addEventListener("click",
+    function studentB(){
+      var student = randomStudent();
+      console.log(student);
+      document.getElementById("studentAnswer").innerHTML = student;
+      document.getElementById("studentAnswer").style.color = "green";
+      document.getElementById("studentA").style.color = "green";
+      return student;
+    }
+);
+
+var gcdButton = document.getElementById("gcdButton").addEventListener("click",
+    function gcdB(){
+      var number0 = parseInt(document.getElementById("gcdInput0").value);
+      var number1 = parseInt(document.getElementById("gcdInput1").value);
+      console.log(gcd(number0, number1));
+      document.getElementById("gcdAnswer").innerHTML = gcd(number0, number1);
+      document.getElementById("gcdAnswer").style.color = "green";
+      document.getElementById("gcdA").style.color = "green";
+      return gcd(number0, number1);
+    }
+);
+
 
 // var fibButton = document.getElementById("fibButton").addEventListener("click", console.log("clicked"));
