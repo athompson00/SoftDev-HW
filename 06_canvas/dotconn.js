d5c7042b8fe2166ff035cab60b0562195c9d2933
athompson00 <athompson00@stuy.edu>
2020-02-11 21:34:35 -0500

    finished assignment

---
+++ 06_canvas/dotconn.js
@@ -0,0 +1,36 @@
+var lastPoint = [0, 0];
+
+var clear = document.getElementById('clear');
+
+var clearCanvas = function(e) {
+  ctx.clearRect(0, 0, 600, 600);
+}
+
+clear.addEventListener("click", clearCanvas);
+
+var playground = document.getElementById("playground");
+
+
+
+var ctx = playground.getContext("2d");
+
+var draw = function(e){
+  //console.log("ran draw");
+  var barrier = playground.getBoundingClientRect();
+  var x = e.clientX - barrier.left;// here I used getBoundingClientRect which gets the boundaries of the selected element (this time being the canvas). e.ffset if the same idea. it gets the x value adjusting for the offset automatically
+  var y = e.clientY - barrier.top;
+  if (lastPoint[0] == 0){
+    ctx.beginPath();
+    lastPoint[0] = x;
+    lastPoint[1] - y;
+  } else {
+    ctx.lineTo(x,y);
+    ctx.stroke();
+  }
+  ctx.beginPath();
+  ctx.arc(x, y, 5, 0, Math.PI * 2);
+  ctx.fill();
+
+}
+
+playground.addEventListener("click", draw);
