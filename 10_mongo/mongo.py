363a98f82373d6ca5547aebc09ca10f2c66d22de
athompson00 <athompson00@stuy.edu>
2020-03-02 22:19:35 -0500

    maybe finished assignment

--- 10_mongo/mongo.py
+++ 10_mongo/mongo.py
@@ -15,24 +15,60 @@ from pymongo import MongoClient
 client = MongoClient()
 db = client.teamName
 collection = db.events
-if(collection.count() == 0):
-    file = open("prize.json", "r")
-    content = file.readlines()
-    for line in content:
-        collection.insert_one(json.loads(line))
+collection.remove()
+#if(collection.count() == 0):
+client = MongoClient()
+db = client.teamName
+collection = db.events
+
+with open("prize.json", "r") as file:
+    content = json.load(file)
+    for line in content['prizes']:
+
+        # print('\n')
+        # print(line["category"])
+        newline = {"year": line["year"], "category": line["category"]}
+        laureates = {}
+        if "laureates" in line:
+            for laureate in line['laureates']:
+                if 'surname' in laureates:
+                    laureates[laureate['id']] = {'firstname': laureate['firstname'], 'surname': laureate['surname'], 'motivation': laureate['motivation']}
+                else:
+                    laureates[laureate['id']] = {'firstname': laureate['firstname'], 'motivation': laureate['motivation']}
+            newline['laureates'] = laureates
+        #print(newline)
+        # print('\n')
+        collection.insert_one(newline)
+
+
+# for document in collection.find({"category": "chemistry"}):
+#     print(document)
+#     print("\n\n")
 
 def findTopic(topic):
     topic = topic.lower()
-    data = collection.find({"prizes.category": topic})
+    data = collection.find({"category": topic})
+    # print(data)
     for thing in data:
-        print(thing)
-        print('\n')
+        for item in thing:
+            if (item != "laureates"):
+                print("{} : {}".format(item, thing[item]))
+            else:
+                for piece in thing[item]:
+                    print("{} : {}".format(piece, thing[item][piece]))
+        print("\n")
 
 def findYear(year):
-    topic = topic.lower()
-    data = collection.find({"prizes.year": year})
+    data = collection.find({"year": year})
+    # print(data[0])
     for thing in data:
-        print(data)
-        print('\n')
+        for item in thing:
+            if (item != "laureates"):
+                print("{} : {}".format(item, thing[item]))
+            else:
+                for piece in thing[item]:
+                    print("{} : {}".format(piece, thing[item][piece]))
+        print("\n")
 
+# findTopic("chemistry")
 findTopic("chemistry")
