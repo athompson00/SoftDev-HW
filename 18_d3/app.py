426ff6bdfede73fe79d6a03bf5fde0f53c567bb1
athompson00 <athompson00@stuy.edu>
2020-04-20 15:22:57 -0400

    completed assignment

---
+++ 18_d3/app.py
@@ -0,0 +1,52 @@
+# Alex Thompsen
+# SoftDev pd 2
+# K18: come up for air
+# 2020-03-05
+
+import json
+import csv
+from flask import Flask, render_template, request, session, redirect, url_for
+
+app = Flask(__name__)
+
+def getscores(year):
+    scores = []
+    schools_list = {}
+    with open('scores' + str(year) + '.json') as json_file:
+        schools = json_file.read()
+        schools_list = json.loads(schools)
+        schools_list = schools_list['data']
+    for school in schools_list:
+        #print(school)
+        if (school[11] != None and school[11] != 's'):
+            # print(school[11])
+            # print(school[12])
+            # print(school[13])
+            # print('\n')
+            scores.append(float(school[11]) + float(school[12]) + float(school[13]))
+    return scores
+
+def cachescores(vals2010, vals2012):
+    with open('static/2010.csv', 'w', newline='') as csvfile:
+        filewriter = csv.writer(csvfile)
+        filewriter.writerow(['score'])
+        for val in vals2010:
+            filewriter.writerow([str(val)])
+    with open('static/2012.csv', 'w', newline='') as csvfile:
+        filewriter = csv.writer(csvfile)
+        filewriter.writerow(['score'])
+        for val in vals2012:
+            filewriter.writerow([str(val)])
+
+
+
+@app.route('/')
+def landing():
+    values2010 = getscores(2010)
+    values2012 = getscores(2012)
+    #cachescores(values2010, values2012)
+    return render_template("index.html", values2010 = values2010, values2012 = values2012)
+
+if __name__ == "__main__":
+        app.debug = True
+        app.run()
---
+++ 18_d3/static/scores.js
@@ -0,0 +1,119 @@
+var sw = false;
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 30, bottom: 30, left: 40},
+    width = 1000 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#hist")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+// get the data
+// d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/1_OneNum.csv", function(data) {
+d3.csv("static/2010.csv", function(data) {
+  console.log(data);
+  // X axis: scale and draw:
+  var x = d3.scaleLinear()
+      .domain([800, 2200])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
+      .range([0, width]);
+  svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+  // set the parameters for the histogram
+  var histogram = d3.histogram()
+      .value(function(d) { return d.score; })   // I need to give the vector of value
+      .domain(x.domain())  // then the domain of the graphic
+      .thresholds(x.ticks(70)); // then the numbers of bins
+
+  //console.log(histogram);
+
+  // And apply this function to data to get the bins
+  var bins = histogram(data);
+
+  console.log(bins);
+
+  // Y axis: scale and draw:
+  var y = d3.scaleLinear()
+      .range([height, 0]);
+      y.domain([0, d3.max(bins, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
+  svg.append("g")
+      .call(d3.axisLeft(y));
+
+  // append the bar rectangles to the svg element
+  svg.selectAll("rect")
+      .data(bins)
+      .enter()
+      .append("rect")
+        .attr("x", 1)
+        .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
+        .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
+        .attr("height", function(d) { return height - y(d.length); })
+        .style("fill", "#69b3a2")
+
+});
+
+var update = function(e){
+  //console.log("ran");
+  var title = document.getElementById('title');
+  var caption = document.getElementById('caption');
+  var filename;
+  console.log(sw);
+  if (!sw){
+    filename = "static/2012.csv";
+    title.innerHTML = " Average SAT scores by school in NYC 2012 (out of 2400)";
+    caption.innerHTML = " Click this button to see 2010 scores in NYC ";
+  } else {
+    filename = "static/2010.csv";
+    title.innerHTML = " Average SAT scores by school in NYC 2010 (out of 2400)";
+    caption.innerHTML = " Click this button to see 2012 scores in NYC ";
+  }
+  sw = !sw;
+  d3.csv(filename, function(data) {
+    console.log(data);
+    var x = d3.scaleLinear()
+        .domain([800, 2200])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
+        .range([0, width]);
+
+
+    var histogram = d3.histogram()
+        .value(function(d) { return d.score; })   // I need to give the vector of value
+        .domain(x.domain())  // then the domain of the graphic
+        .thresholds(x.ticks(70)); // then the numbers of bins
+    var bins = histogram(data);
+    console.log(bins);
+    var y = d3.scaleLinear()
+        .range([height, 0]);
+        y.domain([0, d3.max(bins, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
+
+
+    //svg = d3.select("#hist").transition();
+    //
+    // svg.append("g")
+    //     .attr("transform", "translate(0," + height + ")")
+    //     .call(d3.axisBottom(x));
+    //
+    // svg.append("g").transition()
+    //     .call(d3.axisLeft(y));
+
+  //  console.log(svg);
+    svg.selectAll("rect")
+        .data(bins).transition()
+      //  .enter()
+          .attr("x", 1)
+          .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
+          .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
+          .attr("height", function(d) { return height - y(d.length); })
+
+});
+}
+
+
+
+var button = document.getElementById('transition');
+button.addEventListener("click", update);
